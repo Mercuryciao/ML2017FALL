134a775a293f1cf40938aeb392e2eda74975683b
ming <alcapa@instance-1.c.teng-186116.internal>
2017-11-18 05:09:30 +0000

    gg

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,4 +1,4 @@
-wget https://www.dropbox.com/s/6bg21x217ylyyh4/model-1.h5?dl=1
-wget https://www.dropbox.com/s/ht3tzdg3a79qmeo/model-2.h5?dl=1
-wget https://www.dropbox.com/s/zsixwqeuxlto9fi/model-3.h5?dl=1
+wget -O model-1.h5 https://www.dropbox.com/s/6bg21x217ylyyh4/model-1.h5?dl=1
+wget -O model-2.h5 https://www.dropbox.com/s/ht3tzdg3a79qmeo/model-2.h5?dl=1
+wget -O model-3.h5 https://www.dropbox.com/s/zsixwqeuxlto9fi/model-3.h5?dl=1
 python3 hw3_test.py $1 $2

a33e22d961785c351414d2fec8b02dea3c046c05
ming <alcapa@instance-1.c.teng-186116.internal>
2017-11-18 05:38:34 +0000

    new

--- hw3/hw3_test.py
+++ hw3/hw3_test.py
@@ -3,7 +3,6 @@ import numpy as np
 from random import shuffle
 import argparse
 from math import log, floor
-import pandas as pd
 import csv 
 from keras.utils import np_utils
 from keras.models import Sequential
@@ -20,18 +19,30 @@ output_file = sys.argv[2]
 # output = pd.read_csv('output.csv', sep=',', header=0)
 # voteanswer = pd.read_csv('voteanswer.csv', sep=',', header=0)
 
-cl_1 = load_model('model-1.5h')
+with open(input_file, 'r') as f:
+    data = f.read().strip('\r\n').replace(',', ' ').split()[2:]
+    data = np.array(data)
+    Y_test = []
+    X_test = []
+    for y in range(0,len(data),48*48+1):   
+        X_test.append(data[y+1:y+48*48+1].reshape(48,48,1)) 
+#X = np.array(X).astype(float)/255
+X_test = np.array(X_test).astype(float)/255
+
+
+
+cl_1 = load_model('model-1.h5')
 cl_1.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-predict = cl_1.predict(X)
+predict = cl_1.predict(X_test)
 predict = np.argmax(predict, axis=1)
 voteanswer = predict
-cl_2 = load_model('model-2.5h')
+cl_2 = load_model('model-2.h5')
 
 fieldnames = ['id','label']
-pre_2 = np.argmax(cl_2.predict(X),axis=1)
-cl_3 = load_model('model-3.5h')
+pre_2 = np.argmax(cl_2.predict(X_test),axis=1)
+cl_3 = load_model('model-3.h5')
 voteanswer = np.c_[voteanswer,pre_2]
-pre_3 = np.argmax(cl_3 .predict(X),axis=1)
+pre_3 = np.argmax(cl_3 .predict(X_test),axis=1)
 voteanswer = np.c_[voteanswer,pre_3]
 
 
@@ -45,7 +56,7 @@ fieldnames = ['id','label']
 with open(output_file, 'w') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
     writer.writerow({'id': 'id', 'label': 'label'})
-    for index,line in enumerate(df_new):
+    for index,line in enumerate(voteanswer):
         a = int(np.argmax(np.bincount(line)))
         writer.writerow({'id': index, 'label':a })
 #with open(output_path, 'w') as f:

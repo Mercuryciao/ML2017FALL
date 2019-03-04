783ea29fad1348dbca651acc559f507ed822b734
Mercuryciao <32545127+Mercuryciao@users.noreply.github.com>
2019-03-04 21:45:45 +0800

    Update hw1_train.py

--- hw1/hw1_train.py
+++ hw1/hw1_train.py
@@ -3,50 +3,50 @@ import pandas as pd
 import numpy as np
 import math
 
-data = pd.read_csv('C:/Users/Vicky/Desktop/ML/hw1/train.csv',encoding='gb18030')
-data = data.rename(columns={'代兜': 'item'})
+data = pd.read_csv('C:/Users/Vicky/Desktop/ML/hw1/train.csv',encoding = 'gb18030')
+data = data.rename(columns = {'代兜': 'item'})
 
 #pm25 = data[data['item']=='PM2.5'].drop(['item','data'])
-pm25=data[data['item']=='PM2.5']
-pm25f9 = pm25.iloc[:,4:13].replace('NR',0)
-row=np.ones(240)
-temp=np.c_[row,pm25f9]
+pm25 = data[data['item']=='PM2.5']
+pm25f9 = pm25.iloc[:,4:13].replace('NR', 0)
+row = np.ones(240)
+temp = np.c_[row, pm25f9]
 theta = np.ones(10)
 theta = theta.transpose()
 temp = temp.astype(float)
 
 for i in range(10000):
-    hypothesis=np.dot(temp, theta)
-    y=pm25['10']
+    hypothesis = np.dot(temp, theta)
+    y = pm25['10']
     y = y.astype(float)
     loss = hypothesis - y
-    m=240
+    m = 240
     cost = np.sum(loss ** 2) / m
-    x=temp
+    x = temp
     xTrans = x.transpose()
     gradient = np.dot(xTrans, loss) / m
     #print(math.sqrt(cost))
     alpha = 0.00005
     theta = theta - alpha * gradient
 
-w = np.matmul(np.matmul(inv(np.matmul(x.transpose(),x)),x.transpose()),y)
+w = np.matmul(np.matmul(inv(np.matmul(x.transpose(),x)), x.transpose()), y)
 
 test = pd.read_csv('C:/Users/Vicky/Desktop/ML/hw1/test.csv',header=None)
 test = test[test.iloc[:,1]=='PM2.5']
-name=test[0]
+name = test[0]
 test = test.iloc[0:,2:]
-row=np.ones(240)
-test=np.c_[row,test]
+row = np.ones(240)
+test = np.c_[row, test]
 test = test.astype(float)
-result=np.dot(test, w)
-aa=np.c_[name,result]
-print(len(aa))
+result = np.dot(test, w)
+np_data = np.c_[name, result]
+print(len(np_data))
 filename = "C:/Users/Vicky/Desktop/ML/hw1/handcraft.csv"
 text = open(filename, "w+")
-s = csv.writer(text,delimiter=',')
+s = csv.writer(text,delimiter = ',')
 s.writerow(["id","value"])
-for i in range(len(aa)):
-    s.writerow(aa[i]) 
+for i in range(len(np_data)):
+    s.writerow(np_data[i]) 
 text.close()
 
 
